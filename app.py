c1208c9ee669021f92cfe45025d78c78d53a3837
DeadLoss1801 <109912987+DeadLoss1801@users.noreply.github.com>
2023-08-15 11:29:42 +0000

    api

--- app.py
+++ app.py
@@ -28,9 +28,11 @@ JOBS = [{
 def helloWorld():
   return render_template('home.html', jobs=JOBS, company_name='Microsoft')
 
-@app.route("/jobs")
+
+@app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
